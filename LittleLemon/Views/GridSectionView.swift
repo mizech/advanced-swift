4e485cedf99de46e579cf80f925b2467e10e7956
mizech <michael.zech@hotmail.de>
2024-08-17 12:26:37 +0200

    Add: Logo

---
+++ LittleLemon/Views/GridSectionView.swift
@@ -0,0 +1,18 @@
+//
+//  GridSectionView.swift
+//  LittleLemon
+//
+//  Created by Michael on 17.08.24.
+//
+
+import SwiftUI
+
+struct GridSectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GridSectionView()
+}
